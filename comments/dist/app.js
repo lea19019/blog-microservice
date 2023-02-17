6eecf1d598b962d7110f73586ce5fd77c0f5ac0d
Adrian Castillo <vicacl945@gmail.com>
2023-02-17 16:27:06 -0600

    Added functionality to service to handle comments by post

--- comments/dist/app.js
+++ comments/dist/app.js
@@ -4,6 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const crypto_1 = require("crypto");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-console.log("Running...");
-app.listen(3000);
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+const commentsByPost = {};
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPost[req.params.id] || []);
+});
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = (0, crypto_1.randomBytes)(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentsByPost[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPost[req.params.id] = comments;
+    res.status(201).send(comments);
+});
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
--- comments/src/app.ts
+++ comments/src/app.ts
@@ -1,5 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import { randomBytes } from 'crypto';
+import cors from 'cors';
 
+interface IComment { id: string, content: string }
+
+interface ICommentsByPost {
+    [id: string]: IComment[]
+}
 const app = express();
-console.log("Running...")
-app.listen(3000)
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPost: ICommentsByPost = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPost[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body
+
+    const comments: IComment[] = commentsByPost[req.params.id] || [];
+
+    comments.push({id: commentId, content})
+
+    commentsByPost[req.params.id] = comments;
+
+    res.status(201).send(comments);
+})
+
+app.listen(4001, () => {
+    console.log('Listening on 4001')
+})
