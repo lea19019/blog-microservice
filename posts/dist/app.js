8a939e60545eb66f9be293a73ea0fffa67ef98d9
Adrian Castillo <vicacl945@gmail.com>
2023-02-17 16:27:43 -0600

    Added functionality to retrieve all posts and add posts

--- posts/dist/app.js
+++ posts/dist/app.js
@@ -4,6 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const crypto_1 = require("crypto");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-console.log("Running... Posts");
-app.listen(3000);
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+const posts = {};
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+app.post('/posts', (req, res) => {
+    const id = (0, crypto_1.randomBytes)(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id]);
+});
+app.listen(4000, () => {
+    console.log('LIstening on 4000');
+});
--- posts/src/app.ts
+++ posts/src/app.ts
@@ -1,5 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import { randomBytes } from 'crypto';
+import cors from 'cors';
+
+interface IPosts {
+    [id: string]: {
+        id: string,
+        title: string
+    }
+}
 
 const app = express();
-console.log("Running... Posts")
-app.listen(3000)
+app.use(bodyParser.json())
+app.use(cors());
+
+const posts: IPosts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id, title
+    }
+    
+    res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+    console.log('LIstening on 4000')
+})
