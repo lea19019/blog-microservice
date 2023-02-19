794a25ce4b50f3c76d489b0c2b73f70c3246e315
Adrian Castillo <vicacl945@gmail.com>
2023-02-18 18:25:27 -0600

    Fully working query microservice for moderating comments

--- moderation/dist/app.js
+++ moderation/dist/app.js
@@ -1,9 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
-console.log("Running... Moderation");
-app.listen(3000);
+app.use(body_parser_1.default.json());
+app.post('/events', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { type, data } = req.body;
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+        yield axios_1.default.post("http://localhost:4005/events", {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
+    res.send({});
+}));
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
