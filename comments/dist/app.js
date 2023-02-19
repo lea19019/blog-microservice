94354cdefc80f45cc21a7444574052351005978d
Adrian Castillo <vicacl945@gmail.com>
2023-02-18 18:27:01 -0600

    Fully working comments

--- comments/dist/app.js
+++ comments/dist/app.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const crypto_1 = require("crypto");
 const cors_1 = __importDefault(require("cors"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
@@ -14,13 +15,43 @@ const commentsByPost = {};
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPost[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = (0, crypto_1.randomBytes)(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPost[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPost[req.params.id] = comments;
+    await axios_1.default.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    });
     res.status(201).send(comments);
+});
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPost[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+        await axios_1.default.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        }).catch(err => console.log(err, 'cooment evenet'));
+    }
+    res.send({});
 });
 app.listen(4001, () => {
     console.log('Listening on 4001');
--- comments/src/app.ts
+++ comments/src/app.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios';
 
-interface IComment { id: string, content: string }
+interface IComment { id: string, content: string, [field: string]: string | number }
 
 interface ICommentsByPost {
     [id: string]: IComment[]
@@ -18,17 +19,53 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPost[req.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body
 
     const comments: IComment[] = commentsByPost[req.params.id] || [];
 
-    comments.push({id: commentId, content})
+    comments.push({id: commentId, content, status: 'pending'})
 
     commentsByPost[req.params.id] = comments;
+     
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId, 
+            content,
+            postId: req.params.id,
+            status: 'pending'
+        }
+    })
 
     res.status(201).send(comments);
+})
+
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data
+        const comments = commentsByPost[postId]
+
+        const comment = comments.find(comment => {
+            return comment.id === id
+        })!
+        comment.status = status
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                status,
+                content
+            }
+        }).catch(err => console.log(err, 'cooment evenet'))
+    }
+
+    res.send({})
 })
 
 app.listen(4001, () => {
