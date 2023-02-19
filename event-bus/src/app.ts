c6d67af0a63139768e95d3a2af8f5a62248ad951
Adrian Castillo <vicacl945@gmail.com>
2023-02-18 18:26:28 -0600

    Fully working event-bus

--- event-bus/src/app.ts
+++ event-bus/src/app.ts
@@ -1,5 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
-console.log("Running... Event Bus")
-app.listen(3000)
+app.use(bodyParser.json())
+app.use(cors());
+
+const events: object[] = []
+
+app.post('/events', (req, res) => {
+    const event = req.body
+    
+    events.push(event)
+
+    axios.post('http://localhost:4000/events', event).catch(err => console.log(err.message,'0'));
+    axios.post('http://localhost:4001/events', event).catch(err => console.log(err.message,'1'));
+    axios.post('http://localhost:4002/events', event).catch(err => console.log(err.message,'2'));
+    axios.post('http://localhost:4003/events', event).catch(err => console.log(err.message,'3'));
+
+    res.send({ status: 'Ok' });
+})
+
+app.get('/events', (req, res) => {
+    res.send(events)
+})
+
+app.listen(4005, () => {
+    console.log('Listening on 4005')
+})
