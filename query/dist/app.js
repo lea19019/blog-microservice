b74e697c855d4a4e409281c9d5a7ec7ea081370d
Adrian Castillo <vicacl945@gmail.com>
2023-02-18 18:25:52 -0600

    Fully working query microservice

--- query/dist/app.js
+++ query/dist/app.js
@@ -1,9 +1,64 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const axios_1 = __importDefault(require("axios"));
+const cors_1 = __importDefault(require("cors"));
+const posts = {};
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+    else if (type == 'CommentCreated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content, status });
+    }
+    else if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+        comment.content = content;
+    }
+};
 const app = (0, express_1.default)();
-console.log("Running... Query");
-app.listen(3000);
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
+    console.log(posts);
+    res.send({});
+});
+app.listen(4002, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('Listening on 4002');
+    try {
+        const res = yield axios_1.default.get("http://localhost:4005/events");
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+            handleEvent(event.type, event.data);
+        }
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}));
